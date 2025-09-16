d3af32cd9aa71fabffd95c990df437b5ae620abc
ajayrabadiya24 <ajay.rabadiya@pmentire.com>
2025-09-16 12:03:27 +0530

    added design changes

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <title>@yield('title','Frontend')</title>
     @include('partials.frontend.header')
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}">
     @stack('css')
 </head>
 <body>
